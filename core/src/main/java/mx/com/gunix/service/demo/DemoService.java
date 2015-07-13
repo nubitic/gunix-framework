664477efab35999a91360bbe423ef81ae63f19ae
Jesús González Vences <admin@nubitic.mx>
2015-07-13 22:53:44 +0000

    Se eliminan archivos de la aplicación control-escolar

--- core/src/main/java/mx/com/gunix/service/demo/DemoService.java
+++
@@ -1,9 +0,0 @@
-package mx.com.gunix.service.demo;
-
-import mx.com.gunix.domain.demo.Formulario;
-
-public interface DemoService {
-	public void simulaGuardado(Formulario form);
-
-	public Boolean isValid(Formulario form);
-}
