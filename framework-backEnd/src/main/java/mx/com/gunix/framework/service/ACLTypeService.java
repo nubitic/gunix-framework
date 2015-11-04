00086e2da6ed8373d547ed462003b0a918ac42a5
Jesús González Vences <admin@nubitic.mx>
2015-11-04 03:20:25 +0000

    Se sube clase faltante.

---
+++ framework-backEnd/src/main/java/mx/com/gunix/framework/service/ACLTypeService.java
@@ -0,0 +1,31 @@
+package mx.com.gunix.framework.service;
+
+import java.util.List;
+
+import mx.com.gunix.framework.security.domain.ACLType;
+
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+public interface ACLTypeService<T extends ACLType> {
+
+	@PostAuthorize("hasPermission(returnObject, 'READ')")
+	public T getById(Long id);
+
+	@PostFilter("hasPermission(filterObject, 'READ')")
+	public List<T> getAll();
+
+	@PostFilter("hasPermission(filterObject, 'READ')")
+	public List<T> getByExample(Boolean esMaestro, T ejemplo);
+
+	@PreAuthorize("hasPermission(#original, 'WRITE')")
+	public void update(T original, T nuevo);
+
+	@PreAuthorize("hasPermission(#objeto, 'DELETE')")
+	public void delete(T objeto);
+
+	@PreAuthorize("hasRole('ACL_ADMIN')")
+	public long insert(T objeto);
+
+}
