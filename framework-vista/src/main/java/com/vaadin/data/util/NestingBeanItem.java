01ed4958efcbd6dff4ebf465609da2b5a33a337c
Jesús González Vences <admin@nubitic.mx>
2016-01-21 21:59:58 +0000

    Se inicializan beans anidados.

--- framework-vista/src/main/java/com/vaadin/data/util/NestingBeanItem.java
+++ framework-vista/src/main/java/com/vaadin/data/util/NestingBeanItem.java
@@ -11,6 +11,7 @@ import java.util.Set;
 import org.springframework.beans.BeanUtils;
 
 import com.vaadin.data.Property;
+import com.vaadin.data.Property.ReadOnlyException;
 
 @SuppressWarnings("serial")
 public class NestingBeanItem<BT> extends BeanItem<BT> {
@@ -20,11 +21,12 @@ public class NestingBeanItem<BT> extends BeanItem<BT> {
 		exploreProperties(getItemPropertyIds());
 	}
 
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private void exploreProperties(Collection<?> propertyIds) {
 		Set<String> removedProperties = new HashSet<String>();
 		Set<String> nestedProperties = new HashSet<String>();
 		for (final Object propertyId : propertyIds) {
-			Property<?> p = getItemProperty(propertyId);
+			Property p = getItemProperty(propertyId);
 			Class<?> propertyType = p.getType();
 			if (!BeanUtils.isSimpleProperty(propertyType) && !Iterable.class.isAssignableFrom(propertyType) && !Map.class.isAssignableFrom(propertyType)) {
 				// Enumerate all sub-properties
@@ -33,7 +35,12 @@ public class NestingBeanItem<BT> extends BeanItem<BT> {
 					String qualifiedPropertyId = propertyId + "." + key;
 					nestedProperties.add(qualifiedPropertyId);
 				});
-				removedProperties.add((String) propertyId);
+				try {
+					p.setValue(propertyType.newInstance());
+					removedProperties.add((String) propertyId);
+				} catch (ReadOnlyException | InstantiationException | IllegalAccessException e) {
+					throw new RuntimeException(e);
+				}
 			}
 		}
 
