d738cd3b3ad2b98f8667c782fccd24e469c37d90
Jesús González Vences <admin@nubitic.mx>
2017-05-27 00:52:48 +0000

    Se agrega MimeMessageHelper

--- framework-commons/src/main/java/mx/com/gunix/framework/mail/config/MailConfig.java
+++ framework-commons/src/main/java/mx/com/gunix/framework/mail/config/MailConfig.java
@@ -2,20 +2,24 @@ package mx.com.gunix.framework.mail.config;
 
 import java.util.Properties;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
-import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 
 @Configuration
 public class MailConfig {
 	static final Logger log = Logger.getLogger(MailConfig.class);
 	@Bean
-	public MailSender mailSender() {
+	public JavaMailSender mailSender() {
 		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
 		mailSender.setHost(System.getenv("MAIL_SERVER"));
@@ -45,6 +49,19 @@ public class MailConfig {
 		SimpleMailMessage smm = new SimpleMailMessage();
 		smm.setFrom(System.getenv("MAIL_FROM"));
 		return smm;
+	}
+	
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public MimeMessageHelper mimeMailMessageTemplate() {
+		MimeMessage message = ((JavaMailSender) mailSender()).createMimeMessage();
+		try {
+			MimeMessageHelper helper = new MimeMessageHelper(message, true);
+			helper.setFrom(System.getenv("MAIL_FROM"));
+			return helper;
+		} catch (MessagingException e) {
+			throw new RuntimeException("No fue posible inicializar MimeMessageHelper", e);
+		}
 	}
 
 }
