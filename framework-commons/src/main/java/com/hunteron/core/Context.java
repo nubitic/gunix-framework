f0dd82228004687e43dd1016c9df6ac3b48fc590
Jesús González Vences <admin@nubitic.mx>
2018-07-26 19:24:43 +0000

    Se corrige error al rehacer parte del código ya que tenía multiples fallas/puntos de mejora

--- framework-commons/src/main/java/com/hunteron/core/Context.java
+++ framework-commons/src/main/java/com/hunteron/core/Context.java
@@ -1,6 +1,10 @@
 package com.hunteron.core;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 import mx.com.gunix.framework.security.domain.Funcion;
 
@@ -15,33 +19,42 @@ public enum Context {
 	HOST("BACKEND_HOST", "http://localhost:8081/backEnd"), 
 	VIEW_INDEX_TILE_DEF("VIEW_INDEX_TILE_DEF", "gunix.index"), 
 	VIEW_ENGINE("VIEW_ENGINE", Funcion.ViewEngine.VAADIN.name());
-
+	
+	Logger log = Logger.getLogger(Context.class.getName());
+	
 	private String envVar;
 	private String defaultValue;
+	private javax.naming.Context ic;
 
 	private Context(String envVar, String defaultValue) {
 		this.envVar = envVar;
+		this.defaultValue = defaultValue;
 	}
 
 	public String get() {
-		String value = getFromEnvironment(envVar);
+		if (envVar == null) {
+			return null;
+		}
+		
+		String value = System.getenv(envVar);
+		
+		if (value == null) {
+			try {
+				 Object object = getIc().lookup(envVar);
+				if (object != null) {
+					value = object.toString();
+				}
+			} catch (Throwable logE) {
+				log.log(Level.INFO, "No fue posible obtener el valor para la variable " + envVar + ": " + logE.getMessage());
+			}
+		}
 		return value != null ? value : defaultValue;
 	}
 
-	String getFromEnvironment(final String name) {
-		if (name == null)
-			return null;
-		String envVar = System.getenv(name);
-		if (envVar == null) {
-
-			try {
-				final Object object = ((javax.naming.Context) (new InitialContext().lookup("java:comp/env")))
-						.lookup(name);
-				if (object != null)
-					envVar = object.toString();
-			} catch (final Exception ignore) {
-			}
+	private javax.naming.Context getIc() throws NamingException {
+		if (ic == null) {
+			ic = (javax.naming.Context) new InitialContext().lookup("java:comp/env");
 		}
-		return envVar;
+		return ic;
 	}
 }
